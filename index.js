05a5f70661be38d8ee20cf1f682589aea0e4946b
VictorHugo <vyctorvyctor20@outlook.com>
2024-01-17 01:24:01 -0300

    Projeto Pronto

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Digite o nome do herói aqui")
+let nomeDoHeroi = "Digite aqui o nome do herói"
+let xpHeroi= ("Remova os parênteses e as aspas e adicione um número para definir o xp do herói")
+if(xpHeroi <= 1000){
+    nivel = "Ferro"
+} else if(xpHeroi >= 1001 && xpHeroi <= 2000){
+    nivel = "Bronze"
+}  else if(xpHeroi >= 2001 && xpHeroi <= 5000){
+    nivel = "Prata"
+} else if(xpHeroi >= 5001 && xpHeroi <= 7000){
+    nivel = "Ouro"
+} else if(xpHeroi >=7001 && xpHeroi <= 8000){
+    nivel = "Platina"
+} else if(xpHeroi >= 8001 && xpHeroi <= 9000){
+    nivel = "Ascendente"
+} else if(xpHeroi >= 9001 && xpHeroi <= 10000){
+    nivel = "Imortal"
+} else {
+    nivel = "Radiante"
+} 
+console.log(" O Herói " + nomeDoHeroi + "é dó nível " + nivel)
+
+
+
+
+
+
